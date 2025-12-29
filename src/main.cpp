938ace997f57f5a2c458e197f0079fa8be653fe7
Kevin Masterson <k.m.masterson@gmail.com>
2025-12-29 17:53:32 +0200

    - added G_ARGS parsing - moved variable replacement out to separate functions to remove duplicate code - track player correctly in engines that pass an entity pointer to GAME_CLIENT_COMMAND

--- src/main.cpp
+++ src/main.cpp
@@ -38,8 +38,12 @@ pluginvars_t* g_pluginvars = nullptr;
 gentity_t* g_gents = nullptr;
 intptr_t g_gentsize = 0;
 
-intptr_t g_sayflag = 0;
-intptr_t g_sayclient = 0;
+// which client did the say command
+gentity_t* g_sayplayer = nullptr;
+
+// rotating buffers to return values for engines where the arg is returned from G_ARGV instead of in a passed buffer 
+static char buffers[MAX_INFO_STRING][8];
+static int bufindex = 0;
 
 
 // "safe" strncpy that always null-terminates
@@ -48,6 +52,68 @@ char* strncpyz(char* dest, const char* src, std::size_t count) {
 	dest[count - 1] = '\0';
 	return ret;
 }
+
+
+// scan buf for var, replace with int value
+int replace_var_int(char* buf, intptr_t buflen, const char* var, intptr_t value) {
+	int total = 0;
+	
+	// check for var token
+	char* replace = strstr(buf, var);
+	// this argument contains at least one token
+	while (replace) {
+		// stop 'buf' from being read past the token
+		*replace = '\0';
+
+		// form new string with the token replaced
+		strncpyz(buf, QMM_VARARGS(PLID, "%s%d%s", buf, value, replace + 2), buflen);
+		total++;
+
+		// look for another token
+		replace = strstr(buf, var);
+	}
+	
+	return total;
+}
+
+
+// scan buf for var, replace with string value
+int replace_var_str(char* buf, intptr_t buflen, const char* var, const char* value) {
+	int total = 0;
+	
+	// check for var token
+	char* replace = strstr(buf, var);
+	// this argument contains at least one token
+	while (replace) {
+		// stop 'buf' from being read past the token
+		*replace = '\0';
+
+		// form new string with the token replaced
+		strncpyz(buf, QMM_VARARGS(PLID, "%s%s%s", buf, value, replace + 2), buflen);
+		total++;
+
+		// look for another token
+		replace = strstr(buf, var);
+	}
+	
+	return total;
+}
+
+
+int get_player_health(gentity_t* player) {
+#ifdef GAME_HAS_STAT_HEALTH
+	return g_sayplayer->client->ps.stats[STAT_HEALTH];
+#else
+	return g_sayplayer->health;
+#endif
+}
+
+
+#ifndef GAME_NO_ARMOR
+int get_player_armor(gentity_t* player) {
+	return player->client->ps.stats[STAT_ARMOR];
+}
+#endif
 
 
 C_DLLEXPORT void QMM_Query(plugininfo_t** pinfo) {
@@ -67,10 +133,9 @@ C_DLLEXPORT void QMM_Detach() {
 
 
 C_DLLEXPORT intptr_t QMM_vmMain(intptr_t cmd, intptr_t* args) {
-	// cancel arg checking if a new command is entered before this plugin thinks it
-	// has all the "say" args (sanity checking)
+	// cancel arg checking if a new command is entered
 	if ((cmd == GAME_CONSOLE_COMMAND || cmd == GAME_CLIENT_COMMAND)) {
-		g_sayflag = 0;
+		g_sayplayer = nullptr;
 	}
 
 	// hook client console commands
@@ -81,8 +146,13 @@ C_DLLEXPORT intptr_t QMM_vmMain(intptr_t cmd, intptr_t* args) {
 
 		// player is using a say command, set global flags
 		if (!strncmp(buf, "say", 3)) {
-			g_sayflag = g_syscall(G_ARGC);
-			g_sayclient = args[0];
+#ifdef GAME_CLIENT_COMMAND_HAS_ENT
+			// direct entity pointer to the player
+			g_sayplayer = (gentity_t*)args[0];
+#else
+			// clientNum is 0 based but they occupy entities 1-32 (0 is worldspawn)
+			g_sayplayer = ENT_FROM_NUM(args[0] + 1);
+#endif
 		}
 	}
 
@@ -100,71 +170,65 @@ C_DLLEXPORT intptr_t QMM_syscall(intptr_t cmd, intptr_t* args) {
 	// this is the main workhorse
 	// whenever the mod calls trap_Argv for a say command, this checks each argument for the
 	// existence of a replacable token (like "$h") and then replaces all instances of them
-	if (cmd == G_ARGV) {
-		// if this is an argument for a say command
-		if (g_sayflag) {
-			intptr_t argnum = args[0];
+	if (cmd == G_ARGV && g_sayplayer) {
+		int replaced = 0;
+		
+		intptr_t argnum = args[0];
 #ifdef GAME_ARGV_RETURN
-			// this engine returns a buffer to the mod, so we have to use our own
-			static char buffers[MAX_INFO_STRING][8];
-			static int index = 0;
-			// cycle rotating buffer and clear string
-			index = (index + 1) % 8;
-			char* buf = buffers[index];
-			intptr_t buflen = MAX_INFO_STRING;
-			memset(buf, 0, buflen);
+		// this engine returns a buffer to the mod, so we have to use our own
+		// cycle rotating buffer and clear string
+		bufindex = (bufindex + 1) % 8;
+		char* buf = buffers[bufindex];
+		intptr_t buflen = sizeof(buffers[bufindex]);
+		memset(buf, 0, buflen);
 #else
-			// this engine uses a mod-given buffer/size
-			char* buf = (char*)args[1];
-			intptr_t buflen = args[2];
+		// this engine uses a mod-given buffer/size
+		char* buf = (char*)args[1];
+		intptr_t buflen = args[2];
 #endif
-			QMM_ARGV(PLID, argnum, buf, buflen);
+		// read arg into buf regardless of engine
+		QMM_ARGV(PLID, argnum, buf, buflen);
 
-#ifdef GAME_HAS_STAT_HEALTH
-			int health = ENT_FROM_NUM(g_sayclient)->client->ps.stats[STAT_HEALTH];
-#else
-			int health = ENT_FROM_NUM(g_sayclient)->health;
-#endif
-			// check for "$h" tokens
-			char* replace = strstr(buf, "$h");
-			// this argument contains at least one "$h" token
-			while (replace) {
-				// stop 'buf' from being read past the token
-				*replace = '\0';
+		// check for "$h" tokens, replace with health
+		replace_var_int(buf, buflen, "$h", get_player_health(g_sayplayer));
 
-				// form new string with the token replaced
-				strncpyz(buf, QMM_VARARGS(PLID, "%s%d%s", buf, health, replace + 2), buflen);
-
-				// look for another token
-				replace = strstr(buf, "$h");
-			}
-
-// skip for games that don't have armor
 #ifndef GAME_NO_ARMOR
-			int armor = ENT_FROM_NUM(g_sayclient)->client->ps.stats[STAT_ARMOR];
-			replace = strstr(buf, "$a");
-			while (replace) {
-				// stop 'buf' from being read past the token
-				*replace = '\0';
-
-				// form new string with the token replaced
-				strncpyz(buf, QMM_VARARGS(PLID, "%s%d%s", buf, armor, replace + 2), buflen);
-
-				// look for another token
-				replace = strstr(buf, "$a");
-			}
+		// check for "$a" tokens, replace with armor
+		replace_var_int(buf, buflen, "$a", get_player_armor(g_sayplayer));
 #endif
 
-			// stop checking future args if this is the last arg
-			if (argnum == g_sayflag - 1)
-				g_sayflag = 0;
-
-			// NEVER let this go through for say command args
-			// if this game engine returns a buffer, we need to return ours
-			// otherwise, return value doesn't matter
-			QMM_RET_SUPERCEDE((intptr_t)buf);
-		}
+		// NEVER let this go through for say command args
+		// if this game engine returns a buffer, we need to return ours
+		// otherwise, return value doesn't matter
+		QMM_RET_SUPERCEDE((intptr_t)buf);
 	}
+
+#ifdef GAME_HAS_ARGS
+	// whenever the mod calls Args() for a say command, this checks each argument for the
+	// existence of a replacable token (like "$h") and then replaces all instances of them
+	if (cmd == G_ARGS && g_sayplayer) {
+		// this engine returns a buffer to the mod, so we have to use our own
+		// cycle rotating buffer and clear string
+		bufindex = (bufindex + 1) % 8;
+		char* buf = buffers[bufindex];
+		intptr_t buflen = sizeof(buffers[bufindex]);
+		memset(buf, 0, buflen);
+
+		// read args into buf
+		strncpyz(buf, (const char*)g_syscall(G_ARGS), buflen);
+
+		// check for "$h" tokens, replace with health
+		replace_var_int(buf, buflen, "$h", get_player_health(g_sayplayer));
+
+#ifndef GAME_NO_ARMOR
+		// check for "$a" tokens, replace with armor
+		replace_var_int(buf, buflen, "$a", get_player_armor(g_sayplayer));
+#endif
+
+		// NEVER let this go through for say command args
+		QMM_RET_SUPERCEDE((intptr_t)buf);
+	}
+#endif // GAME_HAS_ARGS
 
 	QMM_RET_IGNORED(1);
 }
