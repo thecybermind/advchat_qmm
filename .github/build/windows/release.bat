f0fa503c4543e2df65bad74e677a62222f4069aa
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-09 00:35:20 +0200

    remove Q2R check since this game doesn't support that

--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,5 +1,5 @@
 for %%x in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
-    if [%%G] NEQ [Q2R] msbuild .\msvc\advchat_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x86
+    msbuild .\msvc\advchat_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x86
     if errorlevel 1 exit /b errorlevel
     msbuild .\msvc\advchat_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x64
     if errorlevel 1 exit /b errorlevel
